d8a654e6a533a811b384a2a48845233b5fec0128
Rodger Lee <rodgerleee@gmail.com>
2020-05-19 16:45:53 -0700

    initial upload

---
+++ otherAos.js
@@ -0,0 +1,10 @@
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "30px 0";
+  } else {
+    document.getElementById("navbar").style.padding = "42px 0";
+  }
+}
